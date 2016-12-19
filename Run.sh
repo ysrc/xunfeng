1207f1287a6b3ed8543c5bc0c0d793ed89bf1594
ihacku <nthazwy@gmail.com>
2016-12-19 20:22:02 +0800

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,5 +1,5 @@
 nohup mongodb/Server/bin/mongod --port 65521 --dbpath=/root/app/mongodb/Data --auth  >db.log&
 nohup python Run.py >web.log&
-nohup python aider/aider.py > aider/aider.log &
+nohup python aider/Aider.py > aider/aider.log &
 nohup python nascan/NAScan.py > nascan/scan.log &
 nohup python vulscan/VulScan.py > vulscan/vul.log &

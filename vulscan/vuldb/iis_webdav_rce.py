89fdcee3de00cf6e9a3157e18993cc624421d77a
wolf <admin@xiyv.net>
2017-03-28 12:34:14 +0800

    新增webdav rce（理论扫描）
    只是理论扫描，出现结果不一定就可以利用成功。

---
+++ vulscan/vuldb/iis_webdav_rce.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+import socket
+import time
+import urllib2
+
+
+def get_plugin_info():
+    plugin_info = {
+        "name": "IIS WebDav RCE",
+        "info": "CVE-2017-7269,Windows Server 2003R2版本IIS6.0的WebDAV服务中的ScStoragePathFromUrl函数存在缓存区溢出漏洞，远程攻击者通过以“If: <http://”开头的长header PROPFIND请求，执行任意代码，进而导致服务器被入侵控制。",
+        "level": "紧急",
+        "type": "代码执行",
+        "author": "wolf@YSRC",
+        "url": "http://www.freebuf.com/vuls/130531.html",
+        "keyword": "tag:iis",
+        "source": 1
+    }
+    return plugin_info
+
+
+def check(ip, port, timeout):
+    try:
+        socket.setdefaulttimeout(timeout)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
+        pay="OPTIONS / HTTP/1.0\r\n\r\n"
+        s.send(pay) 
+        data = s.recv(2048)
+        s.close()
+        if "PROPFIND" in data:
+            return u"可能存在IIS WebDav 远程代码执行漏洞"
+    except:
+        pass
